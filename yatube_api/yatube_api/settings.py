360879253b1d7cbec7dd48d865ba1e1283baef01
Третьяков Николай <nikontra@yandex.ru>
2022-04-01 21:13:44 +0300

    READMEv2

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -97,10 +97,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    
 }
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'AUTH_HEADER_TYPES': ('Bearer',),
-} 
+}
